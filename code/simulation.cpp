0c499d8a726af57d1c9c95ce61e9f5df16d8d27e
brandlan <brandlan@msu.edu>
2025-03-10 18:20:49 -0400

    adding command line options for mesh, still need to work through distance function

--- code/simulation.cpp
+++ code/simulation.cpp
@@ -1,17 +1,35 @@
-#include "mfem.hpp"
+#include "../mfem.hpp"
 #include "Initialize_Geometry.hpp"
 #include "Constants.hpp"
 #include <mpi.h>
+#include <iostream>
+
+// Sample runs:  mpirun -np 4 simulation
+//               mpirun -np 4 simulation -m ../Mesh_3x90_T3.mesh
+//               mpirun -np 4 simulation -m ../Mesh_40x30_3.mesh
+//               mpirun -np 4 ex0p -m ../data/square-disc.mesh -o 2 // MFEM Example
+
 
 int main(int argc, char *argv[]) {
     MPI_Init(&argc, &argv);
+
+    // Default values
+    const char* mesh_file = Constants::mesh_file;
+    int order = Constants::order;
+
+    // Parse command-line options from MFEM
+    mfem::OptionsParser args(argc, argv);
+    args.AddOption(&mesh_file, "-m", "--mesh", "Mesh file to use.");
+    args.AddOption(&order, "-o", "--order", "Finite element polynomial degree.");
+    args.ParseCheck();
     
     // Initialize Mesh & Geometry
     Initialize_Geometry geometry;
-    geometry.InitializeMesh(Constants::mesh_file, MPI_COMM_WORLD, Constants::order);
+    geometry.InitializeMesh(mesh_file, MPI_COMM_WORLD, order);
     geometry.SetupBoundaryConditions();
 
     MPI_Finalize();
     return 0;
 
 }
+
