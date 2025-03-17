795b81d10787cd5749c7eefa230a5a06d12d49b4
brandlan <brandlan@msu.edu>
2025-03-17 14:18:55 -0400

    making changes to handle dsF

--- OOP/simulation.cpp
+++ OOP/simulation.cpp
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
 
     // Initialize Mesh & Geometry
     Initialize_Geometry geometry;
-    geometry.InitializeMesh(Constants::mesh_file, MPI_COMM_WORLD, Constants::order);
+    geometry.InitializeMesh(Constants::mesh_file, Constants::dsF_file, MPI_COMM_WORLD, Constants::order);
     geometry.SetupBoundaryConditions();
 
     // Initialize and Calculate Domain Parameters (psi, pse, AvB, AvP)
