c888d59e42558d731482240675b55ac3bd68a9ad
brandlan <brandlan@msu.edu>
2025-09-22 15:14:24 -0400

    adding SimTypes header

---
+++ BESFEM/include/SimTypes.hpp
@@ -0,0 +1,9 @@
+// include/SimTypes.hpp
+#pragma once
+
+namespace sim {
+
+enum class CellMode   { HALF, FULL };
+enum class Electrode  { ANODE, CATHODE, BOTH }; // BOTH used only for FULL
+
+} // namespace sim
